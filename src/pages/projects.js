8ace5be84fdfd27fee93747db0727beb0bae388a
Brian Hobbs <brianhobbs216@gmail.com>
2020-10-07 11:21:39 -0400

    change

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { graphql } from "gatsby"
 import styled from "styled-components";
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 import Projects from '../components/Projects';
 
 const ProjectsPage = ({data}) => {
